75232f8b3eb73c24c8837534257a96f50c886425
Irineu333 <mmmirineusilva@gmail.com>
2024-07-29 17:35:54 -0300

    refactor: remove view-base example

--- app/src/main/java/com/neoa11y/analyze/App.kt
+++
@@ -1,6 +0,0 @@
-package com.neoa11y.analyze
-
-import android.app.Application
-import com.neoa11y.analyzer.ActivityWatcher
-
-class App : Application()
--- app/src/main/java/com/neoa11y/analyze/MainScreen.kt
+++ app/src/main/java/com/neoa11y/analyze/MainScreen.kt
@@ -1,19 +1,11 @@
 package com.neoa11y.analyze
 
-import android.content.Intent
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.neoa11y.analyze.ui.theme.A11yAnalyzerTheme
@@ -30,33 +21,17 @@ import com.neoa11y.analyze.ui.theme.A11yAnalyzerTheme
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier
-) = Row(
-    horizontalArrangement = Arrangement.SpaceBetween,
-    verticalAlignment = Alignment.CenterVertically,
+) = Box(
+    contentAlignment = Alignment.Center,
     modifier = modifier.fillMaxSize()
 ) {
-
-    val onBackPressedDispatcher =
-        LocalOnBackPressedDispatcherOwner.current
-            ?.onBackPressedDispatcher
-
-    IconButton(
-        onClick = {
-            onBackPressedDispatcher?.onBackPressed()
-        }
-    ) {
-        Icon(
-            Icons.Filled.ArrowBack,
-            contentDescription = "voltar"
-        )
-    }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
         var showText by remember { mutableStateOf(true) }
 
         if (showText) {
-            Text("Compose")
+            Text("Hello, World!")
             Spacer(
                 modifier = Modifier.height(8.dp)
             )
@@ -69,23 +44,6 @@ fun MainScreen(
         ) {
             Text("Button")
         }
-    }
-
-    val context = LocalContext.current
-
-    IconButton(
-        onClick = {
-            context.startActivity(
-                Intent(
-                    context, ViewActivity::class.java
-                )
-            )
-        }
-    ) {
-        Icon(
-            Icons.Filled.ArrowForward,
-            contentDescription = "avançar"
-        )
     }
 }
 
